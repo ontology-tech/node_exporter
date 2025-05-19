476d6dc2777433a7bf6f24729fcf390d9e2d96eb
rongyi <rongyi@onchain.com>
2025-05-19 15:38:11 +0800

    Update build option

--- collector/neo_linux.go
+++ collector/neo_linux.go
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !notime
-// +build !notime
+//go:build !noneo
+// +build !noneo
 
 package collector
 
